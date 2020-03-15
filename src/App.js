270b64f733c4f99ca741126e1cfc9dcf2dffed31
Yvonne Nkr <yvonnkr86@gmail.com>
2020-03-15 22:38:37 +0000

    setup initial routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+import NotFound from './pages/404';
+import AuthPage from './pages/AuthPage';
+import Bookings from './pages/Bookings';
+import Events from './pages/Events';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>App Component</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Redirect from='/' to='/auth' exact />
+        <Route exact path='/auth' component={AuthPage} />
+        <Route exact path='/events' component={Events} />
+        <Route exact path='/bookings' component={Bookings} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
