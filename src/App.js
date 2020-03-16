881e7ba4c9f032b9d8eac14d54d074f2cda05dcf
Yvonne Nkr <yvonnkr86@gmail.com>
2020-03-16 11:30:15 +0000

    added navbar

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ import AuthPage from './pages/AuthPage';
 import Bookings from './pages/Bookings';
 import Events from './pages/Events';
 import './App.css';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect from='/' to='/auth' exact />
-        <Route exact path='/auth' component={AuthPage} />
-        <Route exact path='/events' component={Events} />
-        <Route exact path='/bookings' component={Bookings} />
-        <Route component={NotFound} />
-      </Switch>
+      <>
+        <MainNavigation />
+        <main className='main-content'>
+          <Switch>
+            <Redirect from='/' to='/auth' exact />
+            <Route exact path='/auth' component={AuthPage} />
+            <Route exact path='/events' component={Events} />
+            <Route exact path='/bookings' component={Bookings} />
+            <Route component={NotFound} />
+          </Switch>
+        </main>
+      </>
     </BrowserRouter>
   );
 }
