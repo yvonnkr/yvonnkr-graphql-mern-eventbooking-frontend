9fa75bd06ab465b5fcd7fcd9bdcda9dab343fb85
Yvonne Nkr <yvonnkr86@gmail.com>
2020-03-17 15:10:07 +0000

    cancel booking

--- src/pages/Bookings.js
+++ src/pages/Bookings.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import { AuthContext } from '../context/auth-context';
 import Spinner from './../components/Spinner/Spinner';
+import BookingList from '../components/Bookings/BookingList';
 
 const Bookings = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -66,19 +67,50 @@ const Bookings = () => {
     };
   }, [fetchBookings]);
 
+  const deleteBookingHandler = async bookingId => {
+    setIsLoading(true);
+
+    const reqBody = {
+      query: `
+          mutation {
+            cancelBooking(bookingId: "${bookingId}") {
+            _id
+             title
+            }
+          }
+        `
+    };
+
+    //ajax request
+    try {
+      await axios({
+        url: `${process.env.REACT_APP_BACKEND_URL}/graphql`,
+        method: 'POST',
+        data: reqBody,
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${auth.token}`
+        }
+      });
+
+      const updatedBookings = bookings.filter(
+        booking => booking._id !== bookingId
+      );
+      setBookings(updatedBookings);
+
+      setIsLoading(false);
+    } catch (err) {
+      console.log(err);
+      setIsLoading(false);
+    }
+  };
+
   return (
     <>
       {isLoading ? (
         <Spinner />
       ) : (
-        <ul>
-          {bookings.map(booking => (
-            <li key={booking._id}>
-              {booking.event.title} -{' '}
-              {new Date(booking.createdAt).toLocaleDateString()}
-            </li>
-          ))}
-        </ul>
+        <BookingList bookings={bookings} onDelete={deleteBookingHandler} />
       )}
     </>
   );
